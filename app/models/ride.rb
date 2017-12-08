b932166ea90d13e5ec85172fc762b1f349d4c1d0
Robin Pahlman <robin.pahlman@gmail.com>
2017-12-08 18:44:10 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,25 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :user
+  belongs_to :attraction 
+  
+  def take_ride
+    user = self.user
+    attraction = self.attraction 
+
+    if user.height < attraction.min_height && user.tickets < attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif user.height < attraction.min_height
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    elsif user.tickets < attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    else
+      current_tickets = user.tickets - attraction.tickets
+      current_nausea = user.nausea + attraction.nausea_rating
+      current_happiness = user.happiness + attraction.happiness_rating
+
+      user.update(tickets: current_tickets, nausea: current_nausea, happiness: current_happiness)
+      "Thanks for riding the #{attraction.name}!"
+    end
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-
+  root 'application#home'
+  get '/signin', to: 'sessions#sign_in', as: 'signin'
+  post '/signin', to: 'sessions#create' 
+  delete '/signout', to: 'sessions#destroy'
+  get '/rides/new', to: 'rides#new'
+  post '/rides/new', to: 'rides#create'
+  resources :users, :attractions
 
 end
